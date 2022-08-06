e6dcde1fe840424eb66438aaeec9e28414b247a7
Ксения <ksenia2833@gmail.com>
2022-08-06 16:20:16 +0400

    task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -37,13 +37,54 @@ function worker(arr) {
 function makeWork(arrOfArr, func) {
   let max;
 
-  // Ваш кода
-  // for ...
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    
+  }
+
+  return sum;
+}
+
+//console.log(worker([1, 2, 3])); 
+
+
+function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+  let sumResult;
+  sumResult=worker(arrOfArr[i]);
+      
+      if (sumResult > max) {
+        max = sumResult;
+    }
+  }
+
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let minResalt, maxResalt, sum, resalt;
+  minResalt = Infinity;
+  maxResalt = -Infinity;
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > maxResalt) {
+      maxResalt = arr[i];
+    }
+
+    if (arr[i] < minResalt) {
+      minResalt = arr[i];
+    } 
+  }
+
+  resalt = +(maxResalt-minResalt).toFixed(2);
+  
+  return resalt;
 }
+
