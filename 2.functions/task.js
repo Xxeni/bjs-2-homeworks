b10b146938296829df3e594cd6835897d0260fac
Ксения <ksenia2833@gmail.com>
2022-08-06 16:13:52 +0400

    task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,7 +2,25 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    } 
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
