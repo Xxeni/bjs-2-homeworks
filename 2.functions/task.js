75821efd61c36fe1e3957e6d493110939c5d9b38
Ксения <ksenia2833@gmail.com>
2022-08-08 16:37:04 +0400

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,5 @@
 // Задание 1
+
 function getArrayParams(arr) {
     let min = arr[0], max = arr[0], sum = 0, avg;
     for (let i = 0; i < arr.length; i++) {
@@ -14,7 +15,9 @@ function getArrayParams(arr) {
     return { min: min, max: max, avg: avg };
 }
 
+
   // Задание 2
+
 function worker(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
@@ -33,11 +36,13 @@ function makeWork(arrOfArr, func) {
         max = sumArr;
     } 
     }
-    
+
     return max;
 }
 
+
   // Задание 3
+
 function worker2(arr) {
     let max = arr[0];
     let min = arr[0];
@@ -51,7 +56,6 @@ function worker2(arr) {
     }
     }
 
-    
     difference = Math.abs(max - min);
     return difference;
 }
