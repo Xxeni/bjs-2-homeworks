24191253aeec449f4a1b2df26611363786b376f6
Ксения <ksenia2833@gmail.com>
2022-08-08 16:24:20 +0400

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,90 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  min = Infinity;
-  max = -Infinity;
-  sum = 0;
-
+  let min = arr[0], max = arr[0], sum = 0, avg;
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] > max) {
+    if (arr[i] > max) {
       max = arr[i];
-    }
-
-    if (arr[i] < min) {
-      min = arr[i];
-    } 
+      } else if (arr[i] < min) {
+        min = arr[i];
+      }
+      sum += arr[i];
   }
-
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-
-  avg = +(sum / arr.length).toFixed(2);
+  avg = Number ((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  sum = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    
-  }
-
-  return sum;
-}
-
-//console.log(worker([1, 2, 3])); 
-
-
-function makeWork(arrOfArr, worker) {
   let max = -Infinity;
-
+  
   for (let i = 0; i < arrOfArr.length; i++) {
-  let sumResult;
-  sumResult=worker(arrOfArr[i]);
-      
-      if (sumResult > max) {
-        max = sumResult;
-    }
+    let sumArr = func(arrOfArr[i]);
+    if (sumArr > max) {
+      max = sumArr;
+    } 
   }
-
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let minResalt, maxResalt, sum, resalt;
-  minResalt = Infinity;
-  maxResalt = -Infinity;
-  sum = 0;
+  let max = arr[0];
+  let min = arr[0];
+  let difference = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] > maxResalt) {
-      maxResalt = arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
     }
-
-    if (arr[i] < minResalt) {
-      minResalt = arr[i];
-    } 
   }
 
-  resalt = +(maxResalt-minResalt).toFixed(2);
   
-  return resalt;
+  difference = Math.abs(max - min);
+  return difference;
 }
-
